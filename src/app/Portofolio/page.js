1ead583f54d269dacea0c3b3e29a0a6ba84302de
odayTelbany <oday0595814990@gmail.com>
2023-07-19 12:29:09 +0300

    portofolio page

--- src/app/Portofolio/page.js
+++ src/app/Portofolio/page.js
@@ -1,8 +1,31 @@
+import Image from 'next/image'
 import React from 'react'
+import illustrations from "/public/illustration.png"
+import websites from "/public/websites.jpg"
+import apps from "/public/apps.jpg"
 
 function page() {
   return (
-    <div>page</div>
+    <div>
+      <h1 className='text-6xl font-bold mb-8'>Our Works</h1>
+      <div className='flex-1'>
+        <h3 className='font-bold mb-4'>Choose a gallery</h3>
+        <div className='galleries flex gap-16'>
+          <div className='image relative h-350 w-250 border-4 border-white'>
+            <Image className='object-cover' src={illustrations} fill={true}/>
+            <span className='absolute bottom-1 right-1 font-bold text-2xl hover:text-green-600 cursor-pointer transition duration-250 ease-in'>application</span>
+          </div>
+          <div className='image relative h-350 w-250 border-4 border-white'>
+            <Image className='object-cover' src={websites} fill={true}/>
+            <span className='absolute bottom-1 right-1 font-bold text-2xl hover:text-green-600 cursor-pointer transition duration-250 ease-in'>application</span>
+          </div>
+          <div className='image relative h-350 w-250 border-4 border-white'>
+            <Image className='object-cover' src={apps} fill={true}/>
+            <span className='absolute bottom-1 right-1 font-bold text-2xl hover:text-green-600 cursor-pointer transition duration-250 ease-in'>application</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
