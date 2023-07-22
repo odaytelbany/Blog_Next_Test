97cbd824a6ed2e940644a1a1ad023864a4e04ac4
odayTelbany <oday0595814990@gmail.com>
2023-07-22 16:38:05 +0300

    blog page

--- src/app/Blog/page.js
+++ src/app/Blog/page.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import { PortoItem } from "@/components/PortoItem/PortoItem";
+import React from "react";
 
 function page() {
   return (
-    <div>page</div>
-  )
+    <div>
+      <PortoItem
+        title="Lorem ipsum dolor sit amet consecut oesef elit"
+        src="https://images.freeimages.com/images/large-previews/3b5/whats-next-1361638.jpg"
+        blog={true}
+        reverse={true}
+      />
+
+      <PortoItem
+        title="Lorem ipsum dolor sit amet consecut oesef elit"
+        src="https://images.freeimages.com/images/large-previews/000/pet-cat-1561822.jpg"
+        blog={true}
+        reverse={true}
+      />
+
+      <PortoItem
+        title="Lorem ipsum dolor sit amet consecut oesef elit"
+        src="https://images.freeimages.com/images/large-previews/ab7/gerber-and-rose-2-1544099.jpg"
+        blog={true}
+        reverse={true}
+      />
+    </div>
+  );
 }
 
-export default page
+export default page;
--- src/components/PortoItem/PortoItem.jsx
+++ src/components/PortoItem/PortoItem.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Button from "../Button/Button";
 import Image from "next/image";
 
-export const PortoItem = ({title, src, reverse}) => {
+export const PortoItem = ({title, src, reverse, blog}) => {
   return (
-    <div className={`flex gap-24 h-350 ${reverse ? "flex-row-reverse" : ""}`}>
-      <div className="description m-auto flex-1">
+    <div className={`flex ${blog ? "gap-12 h-280 mb-8" : "gap-24 h-350"} ${reverse ? "flex-row-reverse" : ""}`}>
+      <div className={`description m-auto ${blog ? "basis-2/3" : "flex-1"}`}>
         <h2 className="text-3xl font-bold mb-3">{title}</h2>
         <p className="mb-3">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
@@ -14,7 +14,7 @@ export const PortoItem = ({title, src, reverse}) => {
         </p>
         <Button title="See More" />
       </div>
-      <div className="relative flex-1">
+      <div className={`relative ${blog ? "basis-1/3" : "flex-1"}`}>
         <Image className='object-cover' src={src} fill={true} alt="proto"/>
       </div>
     </div>
