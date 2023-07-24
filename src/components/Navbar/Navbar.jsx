6a165c835ebadca5211753a630ac9b4f1376f89d
odayTelbany <oday0595814990@gmail.com>
2023-07-24 19:36:06 +0300

    toggle dark/light

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -50,7 +50,8 @@ function Navbar() {
             <li>
               <Link
                 href="/Portofolio"
-                className="block py-2 pl-3 pr-4  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark: md:dark:hover:text-green-500 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+                className="block py-2 pl-3 pr-4 
+                 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark: md:dark:hover:text-green-500 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
               >
                 Portofolio
               </Link>
--- src/components/ThemeToggle/ThemeToggle.jsx
+++ src/components/ThemeToggle/ThemeToggle.jsx
@@ -4,10 +4,10 @@ import { ThemeContext } from '@/Context/ThemeContext'
 import React, { useContext } from 'react'
 
 const ThemeToggle = () => {
-  const {mode} = useContext(ThemeContext);
+  const {toggle, mode} = useContext(ThemeContext);
 
   return (
-    <div className='w-11 h-6 rounded-xl flex justify-between items-center p-0.5 border-2 border-green-600 relative'>
+    <div className='w-11 h-6 rounded-xl flex justify-between items-center p-0.5 border-2 border-green-600 relative cursor-pointer' onClick={toggle}>
       <div className='text-xs'>☀️</div>
       <div className='text-xs'>🌙</div>
       <div className={`w-4 h-4 bg-green-600 rounded-full absolute ${mode == "dark" ? "left-0.5" : "right-0.5"}`}></div>
